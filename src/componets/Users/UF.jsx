096b462bf61fc035c977ebc6c445b520c2034bb1
gena_gorokhov <mr.genag@mail.ru>
2021-11-05 16:26:18 +0500

    Function change Class component

--- src/componets/Users/UF.jsx
+++ src/componets/Users/UF.jsx
@@ -3,23 +3,26 @@ import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../assets/images/user.png'
 
-class Users extends React.Component{
-  constructor(props){
-    super(props);
- alert('NEW');
-      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
-      this.props.setUsers(response.data.items)
-  });
+const Users = (props) => {
+
+  let getUsers =()=>{
+  if (props.users.length === 0){
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
+     debugger;
+    props.setUsers(response.data.items)
+    });
+
+}
   }
-    render(){
-        return <div>
-       {this.props.users.map(u => <div key={u.id}>
+    return <div>
+      <button onClick={getUsers}>Get Users</button>
+       {props.users.map(u => <div key={u.id}>
       <span>
           <div>
         <img src={u.photos.small !=null ? u.photos.small: userPhoto} className={styles.userPhoto} alt="" />
           </div>
          <div>
-           {u.followed ?  <button onClick={()=> { this.props.unfollow(u.id)}}>Unfollow</button> : <button onClick={()=> { this.props.follow(u.id)}}>Follow</button>}
+           {u.followed ?  <button onClick={()=> { props.unfollow(u.id)}}>Unfollow</button> : <button onClick={()=> { props.follow(u.id)}}>Follow</button>}
         </div>
       </span>
 
@@ -38,8 +41,6 @@ class Users extends React.Component{
        </span>
        </div>)
          }
-        </div> 
-    }
+        </div>  
 }
-
 export default Users;
