484b3d848d59e26f63eaaac63d6872bce8c97ae6
gena_gorokhov <mr.genag@mail.ru>
2021-12-11 20:23:43 +0500

    v.86.01. useState()

---
+++ src/componets/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+
+const ProfileStatusWithHooks=(props)=>{
+
+  let [editMode, setEditMode]=useState(false);
+let[status, setStatus]=useState(props.status)
+
+  const activateEditMode =()=>{
+    console.log("huy");
+setEditMode(true);
+}
+
+const deactivateEditMode=()=>{
+    console.log('deactivate');
+setEditMode(false);
+props.updateStatus(status)
+
+}
+
+const onStatusChange=(e)=>{
+setStatus(e.currentTarget.value);
+}
+return(
+<div>
+   { !editMode && <div>
+        <span onDoubleClick={activateEditMode}>{props.status || "-----"}</span>
+    </div>}
+   {editMode && <div>
+        <input  
+        onChange={onStatusChange}
+        autoFocus={true}
+         onBlur={deactivateEditMode}
+         value={status}/>
+    </div>}
+</div>
+);
+
+}
+
+
+export default ProfileStatusWithHooks;
